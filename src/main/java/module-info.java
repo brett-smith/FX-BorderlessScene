d3d98e471a6cd48d76f5cadab132b167766607ed
Brett Smith <tanktarta@gmail.com>
2023-02-23 14:20:29 +0000

    Removed MultiJar. Release version.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.goxr3plus.fxborderlessscene {
 	requires transitive javafx.controls;
 	requires transitive javafx.fxml;
 	requires java.logging;
-	requires transitive com.sun.jna;
-	requires transitive com.sun.jna.platform;
 	exports com.goxr3plus.fxborderlessscene.borderless;
 	exports com.goxr3plus.fxborderlessscene.window;
 	opens com.goxr3plus.fxborderlessscene.borderless;
